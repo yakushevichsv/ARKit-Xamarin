ddd75c6f09a786be9adde3430832b5040d8b76c9
Siarhei Yakushevich <yakushevichsv@hotmail.com>
2018-12-28 12:19:34 +0300

    ARSceneViewHandler: Dictionary was deleted. ARNativePortal.csproj: Constants were added to the project.

--- Models/ARSceneVIewHandler.cs
+++ Models/ARSceneVIewHandler.cs
@@ -10,53 +10,14 @@ namespace ARNativePortal.Models
 {
     public class ARSceneViewHandler: ARSCNViewDelegate
     {
-        private readonly Dictionary<UUID, SCNNode> anchorNodeDic = new Dictionary<UUID, SCNNode>();
-        /*public override SCNNode GetNode(ISCNSceneRenderer renderer, ARAnchor anchor)
-        {
-            if (anchor is ARPlaneAnchor planeAnchor)
-            {
-                Debug.WriteLine("!!! GetNode with plane anchor: {0}", planeAnchor);
-                var uuid = anchor.Identifier;
-                if (anchorNodeDic.TryGetValue(uuid, out SCNNode retNode))
-                    return retNode;
-
-                var plane = new SCNPlane();
-                var material = SCNMaterial.Create();
-                material.DoubleSided = true;
-                material.Diffuse.ContentColor = UIKit.UIColor.Red.ColorWithAlpha(0.4f);
-                plane.FirstMaterial = material;
-
-                retNode = new SCNNode
-                {
-                    Name = Constants.PlaneNodeName,
-                    Geometry = plane
-                };
-
-                Debug.WriteLine("!!!Return Node {0} Transform {1}", retNode.WorldPosition, retNode.WorldTransform);
-
-                var node = new SCNNode();
-                node.AddChildNode(retNode);
-                TryToAdjustNode(node, anchor);
-                anchorNodeDic[uuid] = node;
-                return node;
-            }
-
-            return base.GetNode(renderer, anchor);
-        } */
-
         private bool TryToAdjustNode(SCNNode node, ARAnchor anchor)
         {
-            if (IsTrackedAnchor(anchor) && (anchor is ARPlaneAnchor planeAnchor))
+            if (anchor is ARPlaneAnchor planeAnchor)
             {
                 AdjustPlaneNode(node, planeAnchor);
                 return true;
             }
             return false;
-        }
-
-        private bool IsTrackedAnchor(ARAnchor anchor)
-        {
-            return true;//anchorNodeDic.ContainsKey(anchor.Identifier);
         }
 
         private static void AdjustPlaneNode(SCNNode node, ARPlaneAnchor planeAnchor)
@@ -114,19 +75,7 @@ namespace ARNativePortal.Models
 
         public override void DidRemoveNode(ISCNSceneRenderer renderer, SCNNode node, ARAnchor anchor)
         {
-            if (anchor is ARPlaneAnchor planeAnchor)
-            {
-                Debug.WriteLine("DidRemoveNode with plane anchor: {0}", planeAnchor);
-                var uuid = anchor.Identifier;
-                if (anchorNodeDic.ContainsKey(uuid))
-                {
-                    anchorNodeDic.Remove(uuid);
-                }
-            }
-            BeginInvokeOnMainThread(() =>
-            {
-                node.RemoveFromParentNode();
-            });
+            BeginInvokeOnMainThread(node.RemoveFromParentNode);
         }
     }
 }
